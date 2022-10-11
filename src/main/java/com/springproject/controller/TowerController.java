8119e3cdd596d520868ba787864b2f502f4b3275
Damey13 <dameyabraham@gmail.com>
2022-10-11 16:13:18 +0530

    new repo

---
+++ src/main/java/com/springproject/controller/TowerController.java
@@ -0,0 +1,70 @@
+package com.springproject.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springproject.exception.RNFException;
+import com.springproject.exception.RNFException;
+import com.springproject.model.Tower;
+import com.springproject.repository.TowerRepository;
+
+@RestController
+@RequestMapping()
+public class TowerController {
+
+	@Autowired
+	private TowerRepository towerRepository;
+
+	@GetMapping("towers")
+	public List<Tower> getAlltower() {
+		return this.towerRepository.findAll();
+	}
+
+	@GetMapping("/towers/{id}")
+	public ResponseEntity<Tower> getTowerById(@PathVariable(value = "id") Long towerId)
+			throws RNFException {
+		Tower tower = towerRepository.findById(towerId)
+				.orElseThrow(() -> new RNFException("Tower not found for this id ::" + towerId));
+		return ResponseEntity.ok().body(tower);
+	}
+
+	@PostMapping("towers")
+	public Tower createTower(@RequestBody Tower tower) {
+		return this.towerRepository.save(tower);
+	}
+
+	@PutMapping("towers/{id}")
+	public ResponseEntity<Tower> updateTower(@PathVariable(value = "id") Long towerId,
+			@Validated @RequestBody Tower towerDetails) throws RNFException {
+		Tower tower = towerRepository.findById(towerId)
+				.orElseThrow(() -> new RNFException("Tower not found for this id - " + towerId));
+		tower.setEmail(towerDetails.getEmail());
+		tower.setFirstName(towerDetails.getFirstName());
+		tower.setLastName(towerDetails.getLastName());
+		return ResponseEntity.ok(this.towerRepository.save(tower));
+	}
+
+	@DeleteMapping ("towers/{id}")
+	public Map<String,Boolean> deleteTower(@PathVariable(value = "id") Long towerId) throws RNFException
+	{
+		Tower tower = towerRepository.findById(towerId)
+				.orElseThrow (() -> new RNFException ("Tower not found for this id -:" + towerId));
+	this.towerRepository.delete(tower);
+	Map<String, Boolean> response = new HashMap<>();
+	response.put("deleted", Boolean.TRUE);
+	return response;
+	}
+}
---
+++ src/main/java/com/springproject/repository/TowerRepository.java
@@ -0,0 +1,11 @@
+package com.springproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springproject.model.Tower;
+
+@Repository
+public interface TowerRepository extends JpaRepository<Tower,Long>{
+
+}
